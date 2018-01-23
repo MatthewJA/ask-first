fa5952fd2f881c4c84f7eb495ea0ae7d1fda4b97
MatthewJA <matthew.alger@gmail.com>
2018-01-23 14:43:33 +1100

    Moved everything into an installable Python project

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ask_first',
+      version='0.1.0',
+      description='Query FIRST',
+      url='http://github.com/MatthewJA/ask-first',
+      author='Matthew Alger',
+      author_email='matthew.alger@anu.edu.au',
+      license='BSD-3',
+      packages=['ask_first'],
+      zip_safe=False)
---
+++ tests/test_ask_first.py
@@ -0,0 +1,39 @@
+"""Tests for ask_first.py.
+
+Matthew Alger <matthew.alger@anu.edu.au>
+Research School of Astronomy and Astrophysics
+The Australian National University
+2018
+"""
+
+from __future__ import print_function, division
+
+import os.path
+import unittest
+
+import numpy
+
+import ask_first
+
+# Path to directory containing this code.
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Path to directory containing the test data.
+DATA_PATH = os.path.join(THIS_DIR, 'data')
+
+
+class TestGetImage(unittest.TestCase):
+
+    def test(self):
+        # These coordinates are the defaults for the FIRST cutout server.
+        coords = 162.5302917, 30.6770889
+        width = 3 / 60
+        im = ask_first.get_image(coords, width, DATA_PATH)
+        reference_im = numpy.load(os.path.join(
+            DATA_PATH,
+            'test_data_162.5302917_30.6770889.npy'))
+        numpy.testing.assert_allclose(reference_im, im)
+
+
+if __name__ == '__main__':
+    unittest.main()
